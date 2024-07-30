314790a7cef8c8da2770aa5911aadc8f85b22cf0
Dev002 - Joao Paulo <ddev02@domtec.svr>
2024-07-30 16:16:56 -0300

    correcao

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lista de Contatos',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
